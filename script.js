781adf0eb52808f4aba6dfa65394b04428a49b22
Audrey Vazzana <avazzana314@gmail.com>
2023-11-06 13:17:28 -0600

    Form is now fully functional. I'm so proud

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ var form = document.getElementById("interestform");
 form.addEventListener("submit", function (e){
     e.preventDefault();
     var formData = {};
-    console.log(form.elements);
+    var ensembles = [];
         for (var i = 0; i < form.elements.length; i++) {
+            console.log(form.elements[i]);
             var element = form.elements[i];
-            if (element.name) {
+            if (element.name && element.name != "ensemble") {
                 formData[element.name] = element.value;
             }
         }
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        for (var i = 0; i < checkboxes.length; i++) {
+          if (checkboxes[i].checked) {
+            ensembles.push(checkboxes[i].value);
+          }
+        }
+        formData["ensembles"] = ensembles;
     var jsonOutput = document.getElementById("jsonOutput");
     jsonOutput.textContent = JSON.stringify(formData, null, 2);
 });
