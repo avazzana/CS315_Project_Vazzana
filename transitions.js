0d0033893f61b98588ef0cc85af01daac16991db
Audrey Vazzana <avazzana314@gmail.com>
2023-11-08 22:55:57 -0600

    final commit for tonight. Finished pages and added mobile css file

--- transitions.js
+++ transitions.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     triangles.forEach((triangle) => {
       const rowContent = triangle.closest(".row").querySelector(".row-content");
       let isAnimating = false;
-  
+    
+      //this is the code for the triangle animation. The FAQs section is set up so that if you click on a triangle, it rotates as the menu expands
       triangle.addEventListener("click", function() {
         if (!isAnimating) {
           triangle.style.animation = "rotateTriangle 0.5s linear forwards";
